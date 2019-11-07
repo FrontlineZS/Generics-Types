b8ad5dc29bc69e854c8fcfea547229af5535292f
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-11-07 04:56:53 +0100

    first commit

---
+++ src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
@@ -0,0 +1,13 @@
+package com.kochaniuk.genericstypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericsTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericsTypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kochaniuk/genericstypes/GenericsTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kochaniuk.genericstypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericsTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
