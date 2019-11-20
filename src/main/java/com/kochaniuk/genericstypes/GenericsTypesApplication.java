5fe48cdba8f4fc70aaf7d476b7f29f60967d8034
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-11-20 14:12:05 +0100

    visibility and usage of type parameters in class / method / interface contexts

--- src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
+++ src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
@@ -1,13 +1,95 @@
 package com.kochaniuk.genericstypes;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
-@SpringBootApplication
-public class GenericsTypesApplication {
+public class GenericsTypesApplication<E> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GenericsTypesApplication.class, args);
-	}
+    // ## CLASS CONTEXT ##
+    // can be used in every non-static construction
+    // * non-static methods
+    // * non-static initializers
+    // * nested classes
+    private E first = null;
+    public E second = null;
 
+    public E method(E argument) {
+        return argument;
+    }
+
+    {
+        E temporary = null;
+    }
+
+    protected class NestedClass {
+        private E first = null;
+        public E second = null;
+        protected E third = null;
+
+        public E method(E argument) {
+            return argument;
+        }
+    }
+
+    // ## METHOD CONTEXT ##
+    // visibility of parametrized type in entire block of method
+    // regardless of whether the method is static or not
+    // same in nested classes defined within the method
+    public static <T> T staticMethod(T argument) {
+        List<T> list = null;
+        T temporary = null;
+
+        class NestedClass {
+            List<T> list = null;
+            T temporary = null;
+
+            public void method(T arg) {
+            }
+        }
+
+        return argument;
+    }
+
+    public <T> T nonStaticMethod(T argument) {
+        List<T> list = null;
+        T temporary = null;
+
+        class NestedClass {
+            List<T> list = null;
+            T temporary = null;
+
+            public void test(T arg) {
+            }
+        }
+
+        return argument;
+    }
+
+    // When type parameters can not occur
+    public static E staticField = null;
+
+    static {
+        E temporary = null;
+    }
+
+    private static E invalidMethod(E argument) {
+        return argument;
+    }
+
+    static class StaticNestedClass {
+        public E field = null;
+    }
+
+    enum SomeEnum {
+        ONE, TWO;
+
+        public void checkSomething(E element) {
+        }
+    }
+
+    public interface NestedInterface {
+        void interfaceMethod(E argument);
+    }
+
+    public static void main(String[] args) {
+    }
 }
