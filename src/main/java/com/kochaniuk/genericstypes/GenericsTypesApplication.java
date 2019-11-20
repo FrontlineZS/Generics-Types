386c38d466a7d6bd4a8178aab3742803da330b45
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-11-20 12:27:40 +0100

    parametrized methods

--- src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
+++ src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
@@ -1,5 +1,6 @@
 package com.kochaniuk.genericstypes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenericsTypesApplication {
@@ -23,6 +24,24 @@ public class GenericsTypesApplication {
         return null;
     }
 
+    private static <T> void print(T object) {
+        System.out.println(object);
+    }
+
+    private static <T> void reverse(List<T> list) {
+        for (int i = 0; i < list.size() / 2; i++) {
+            T element = list.get(i);
+
+            list.set(i, list.get(list.size() - i - 1));
+            list.set(list.size() - i - 1, element);
+        }
+    }
+
+    private static <T extends Number> double sumOfTheFirst(List<T> first, List<T> second) {
+        return first.get(0).doubleValue() +
+            second.get(0).doubleValue();
+    }
+
     public static void main(String[] args) {
         List<Integer> integerList = Utilities.getIntegersList();
 
@@ -33,6 +52,20 @@ public class GenericsTypesApplication {
         System.out.println(
             middleOne(integerList)
         );
+
+        print("\nstring");
+        print(5);
+        print(null);
+        print(new ArrayList<>());
+
+        print("\n" + integerList);
+        reverse(integerList);
+        print(integerList);
+
+        print("\n" + sumOfTheFirst(Utilities.getNumbersList(), new ArrayList<Number>() {{
+            add(100);
+            add(1);
+        }}));
     }
 
 }
