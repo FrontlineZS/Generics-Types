c8ac48fccbc1874ce90d1fd2c1d53a6446d93e3e
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-11-21 13:52:24 +0100

    Arguments of generics types

--- src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
+++ src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
@@ -1,13 +1,40 @@
 package com.kochaniuk.genericstypes;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.sun.jmx.remote.internal.ArrayQueue;
 
-@SpringBootApplication
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class GenericsTypesApplication {
+    private static <T> void print(T argument) {
+        System.out.println(argument);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GenericsTypesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        print("string");
+        print(5);
+//        this.<String>print("string"); requires main non-static
+
+        List<List<?>> heterogeneousList = new ArrayList<>();
+        heterogeneousList.add(new ArrayList<String>());
+        heterogeneousList.add(new LinkedList<Integer>());
+        heterogeneousList.add(new ArrayQueue<>(3));
+
+        List<? extends List<?>> list = new ArrayList<>();
+        list = new ArrayList<List<String>>();
+        list = new ArrayList<List<Number>>();
+
+        List<List<?>> referencja1;
+        List<? extends List<?>> referencja2;
+
+        referencja1 = heterogeneousList;
+        referencja1 = list;
+
+        referencja2 = heterogeneousList;
+        referencja2 = list;
+    }
+
 
 }
