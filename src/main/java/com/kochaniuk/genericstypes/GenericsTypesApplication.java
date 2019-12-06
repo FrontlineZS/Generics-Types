040b914d3680d23f041702cc7e33b199b98a5dfb
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-12-06 12:30:54 +0100

    using reflection for obtaning and providing type information

--- src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
+++ src/main/java/com/kochaniuk/genericstypes/GenericsTypesApplication.java
@@ -1,98 +1,112 @@
 package com.kochaniuk.genericstypes;
 
-import org.apache.logging.log4j.util.Strings;
-
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class GenericsTypesApplication {
-
-    private static ArrayList<String> nonGenericMethodCreatingArrayList() {
-        return new ArrayList<>();
-    }
+public class GenericsTypesApplication<E extends Number & Comparable> {
 
     /*
-    	Ad. #1 __
-    	it is not optimal because it must have an object of this type
-    	in addition, casting is necessary
+        Static information are related to the method of declaring class fields, method or call arguments
+        they are retained during program execution and can be accessed
+        additionally are returned in one of the classes implementing the Type interface __ therefore they are not erased at all
      */
-    @SuppressWarnings("unchecked")
-    private static <T> T methodCreatingCustomObjectFromExistingOne(T existingObject) throws IllegalAccessException, InstantiationException {
-        return (T) existingObject.getClass().newInstance();
-    }
+    private static List<String> astrings = new ArrayList<>();
+    private static List bstrings = new ArrayList<String>();
+    private static List cstrings = new ArrayList();
+    private static ArrayList<String> dstrings = new ArrayList<String>();
+
+    private static String string = "string";
 
     /*
-        Ad. #3 __
-        here we pass object Class
-        in addition, casting is unnecessary
+        GenericsTypesApplication made generic
      */
-    private static <T> T methodCreatingCustomObjectFromClassTypeInformation(Class<T> type) throws IllegalAccessException, InstantiationException {
-        return type.newInstance();
+    private E field;
+
+    public <K, V> E genericMethod() {
+        return field;
     }
 
-	/**
-	 *
-	 * @param type
-	 * @param object
-	 * @param <T>
-	 * @return {@see null} if could not cast object
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 */
-	/*
-        Ad. #3 __
-        here we pass object Class and some Object
-        in addition, casting is unnecessary
-     */
-    private static <T> T methodCreatingCustomObjectFromClassTypeInformationAndObject(Class<T> type, Object object) throws IllegalAccessException, InstantiationException {
-        // if (object instanceof T) {} cannot use this so other way:
+    public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException {
+        System.out.println("\nClasses");
 
-        if (type.isInstance(object)) {
-            return type.cast(object);
+        Class<?> aClass = astrings.getClass();
+        System.out.println(aClass);
+
+        Class<?> bClass = bstrings.getClass();
+        System.out.println(bClass);
+
+        Class<?> cClass = cstrings.getClass();
+        System.out.println(cClass);
+
+        Class<?> dClass = dstrings.getClass();
+        System.out.println(dClass);
+
+        Class<?> stringClass = string.getClass();
+
+        isClazzParametrized(aClass); // Type class java.util.ArrayList is parametrized, name: ArrayList
+        isClazzParametrized(bClass); // Type class java.util.ArrayList is parametrized, name: ArrayList
+        isClazzParametrized(cClass); // Type class java.util.ArrayList is parametrized, name: ArrayList
+        isClazzParametrized(dClass); // Type class java.util.ArrayList is parametrized, name: ArrayList
+        isClazzParametrized(stringClass);
+
+        isTypeParametrized(GenericsTypesApplication.class.getDeclaredField("astrings").getGenericType(), "astrings");
+        isTypeParametrized(GenericsTypesApplication.class.getDeclaredField("bstrings").getGenericType(), "bstrings");
+        isTypeParametrized(GenericsTypesApplication.class.getDeclaredField("cstrings").getGenericType(), "cstrings");
+        isTypeParametrized(GenericsTypesApplication.class.getDeclaredField("dstrings").getGenericType(), "dstrings");
+
+        /*
+            GenericsTypesApplication made generic
+        */
+        Method genericMethod = GenericsTypesApplication.class.getMethod("genericMethod"); // must be public
+        Type type = genericMethod.getGenericReturnType();
+        System.out.println("\n" + type);
+        System.out.println("toString() - " + genericMethod.toString());
+        System.out.println("toGenericString() - " + genericMethod.toGenericString());
+
+        TypeVariable[] parameters = genericMethod.getTypeParameters();
+        for (TypeVariable parameter : parameters) {
+            System.out.println("\ntype parameter: " + parameter);
         }
 
-        return null;
+        GenericsTypesApplication<SomeNumberClass> ourClassParametrizedAsInteger = new GenericsTypesApplication<>();
+        Class<?> clazz = ourClassParametrizedAsInteger.getClass();
+
+        for (TypeVariable parameter : clazz.getTypeParameters()) {
+            System.out.println("\nParameter name: " + parameter.getName());
+            System.out.println("Parameter generic declaration: " + parameter.getGenericDeclaration());
+
+            Type[] bounds = parameter.getBounds();
+            for (Type t : bounds) {
+                System.out.println("\n" + t);
+            }
+        }
+
     }
 
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        ArrayList<String> strings = nonGenericMethodCreatingArrayList();
+    private static void isClazzParametrized(Class clazz) {
+        if (clazz.getTypeParameters().length > 0) {
+            System.out.println(String.format("\nType %s is parametrized", clazz));
+        }
+    }
 
-        /*
-        	ArrayList<Number> numbers = nonGenericMethodCreatingArrayList();
-        	String object = nonGenericMethodCreatingArrayList();
-
-        	it would be great if you could create any parameterization for the ArrayList class
-
-        	for e.g.
-
-			private static <T> T genericMethodCreatingArrayList() {
-				return new T();  UNFORTUNATELY __ we cannot create new instance using 'new' keyword if the parameter is type parameter ( T )
-			}
-
-			but there are so other ways to do this:
-
-			1. we can pass the final object of the expected type, but this is a waste because it is not used anywhere
-			2. an alternative is to pass an array built with elements of this type and a similar situation because we don't need de facto objects, only type information
-			3. it seems the most sensible to use a Class object parameterized by some type. It is generic type introduced in Java 5
-         */
-
-        ArrayList<String> strings2 = methodCreatingCustomObjectFromExistingOne(new ArrayList<>());
-        ArrayList<Number> numbers = methodCreatingCustomObjectFromExistingOne(new ArrayList<>());
-        String string = methodCreatingCustomObjectFromExistingOne(Strings.EMPTY);
-        String string2 = methodCreatingCustomObjectFromExistingOne("Some string");
-
-        /*
-        	Ad. #3 examples
-         */
-        ArrayList<String> stringsFromClassType = methodCreatingCustomObjectFromClassTypeInformation(ArrayList.class);
-        ArrayList<Number> numbersFromClassType = methodCreatingCustomObjectFromClassTypeInformation(ArrayList.class);
-        String stringFromClassType = methodCreatingCustomObjectFromClassTypeInformation(String.class);
-
-		/*
-			instanceof do not work well with generics types __ actually is worthless
-		 */
-		ArrayList<String> stringsFromClassTypeAndObject = methodCreatingCustomObjectFromClassTypeInformationAndObject(ArrayList.class, new Object());
-		ArrayList<Number> numbersFromClassTypeAndObject = methodCreatingCustomObjectFromClassTypeInformationAndObject(ArrayList.class, new Object());
-		String stringFromClassTypeAndObject = methodCreatingCustomObjectFromClassTypeInformationAndObject(String.class, new Object());
+    private static void isTypeParametrized(Type type, String declaredFieldName) {
+        if (type instanceof ParameterizedType) {
+            System.out.println(String.format(
+                "\nType %s called %s - is parametrized",
+                declaredFieldName,
+                type.getTypeName())
+            );
+        } else {
+            System.out.println(String.format(
+                "\nType %s called %s - is row type",
+                declaredFieldName,
+                type.getTypeName())
+            );
+        }
     }
 
 }
---
+++ src/main/java/com/kochaniuk/genericstypes/SomeNumberClass.java
@@ -0,0 +1,28 @@
+package com.kochaniuk.genericstypes;
+
+public class SomeNumberClass extends Number implements Comparable<GenericsTypesApplication> {
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(GenericsTypesApplication o) {
+        return 0;
+    }
+}
